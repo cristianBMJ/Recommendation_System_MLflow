0bc2fa6e5f8ffb8814bfccee48f12c55882ce49c
CristianBMJ <cpmartnez@ug.uchile.cl>
2024-05-27 19:38:57 -0400

    Log first experiment in mlflow

--- deploy/save_model.py
+++ deploy/save_model.py
@@ -1,4 +1,7 @@
 import joblib
+from surprise import Dataset, Reader, SVD # see dataset into surprise or use it others
+
+algo =SVD()
 
 joblib.dump(algo, 'recommender_model.pkl')
 
--- model/model.py
+++ model/model.py
@@ -1,10 +1,18 @@
-from surprise import Dataset, Reader, SVD
+from surprise import Dataset, Reader, SVD # see dataset into surprise or use it others
 from surprise.model_selection import train_test_split
 from surprise import accuracy
 
+import pandas as pd 
+
+df = pd.DataFrame({
+    'customer_id': [1, 2, 3, 4, 5],
+    'item_id': [1, 2, 3, 4, 5],
+    'rating': [5, 3, 4, 2, 1]
+})
+
 # Load the dataset
 reader = Reader(rating_scale=(1, 5))
-data = Dataset.load_from_df(df[['customer_id', 'product_id', 'rating']], reader)
+data = Dataset.load_from_df(df[['customer_id', 'item_id', 'rating']], reader) # load dataset
 
 # Split the dataset into training and testing
 trainset, testset = train_test_split(data, test_size=0.25)
