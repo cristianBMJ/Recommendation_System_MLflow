fce5e606913b175a9fc7c6b8b2ff75dfd15b5e5e
CristianBMJ <cpmartnez@ug.uchile.cl>
2024-06-11 19:24:43 -0400

    Add Load_and_preprocess_data function

--- main.py
+++ main.py
@@ -10,21 +10,24 @@ mlflow.set_tracking_uri("sqlite:///mlflow.db")
 mlflow.set_experiment("Recommender System Class: SVD, KnnBasic, NormalPredictor")
 
 # Load and preprocess data
-file = "./data/datasets_amazon/Automotive_5_core.csv"
-df_full = pd.read_csv(file)
-df_full_sample = df_full.sample(frac=0.001)
 
-reader = Reader(rating_scale=(1, 5))
-data = Dataset.load_from_df(df_full_sample[['user_id', 'parent_asin', 'rating']], reader)
-trainset, testset = train_test_split(data, test_size=0.25)
+def load_and_preprocess_data(file_path, sample_frac=0.001):
+    df_full = pd.read_csv(file_path)
+    df_sample = df_full.sample(frac=sample_frac)
+    reader = Reader(rating_scale=(1, 5))
+    data = Dataset.load_from_df(df_sample[['user_id', 'parent_asin', 'rating']], reader)
+    return data
+
+file_path = "./data/datasets_amazon/Automotive_5_core.csv"
+data = load_and_preprocess_data(file_path)
 
 # Train SVD model
-model_svd = SVD()
+model_svd = SVD() # Singular Value Decomposition
 svd_recommender = RecommenderSystem(data, model_svd, "SVD")
 svd_recommender.train_model()
 
 # Train KNN model
-sim_options = {'name': 'cosine', 'user_based': True}
+sim_options = {'name': 'cosine', 'user_based': False} # similariy between Users, False between Items
 model_knn = KNNBasic(sim_options=sim_options)
 knn_recommender = RecommenderSystem(data, model_knn, "KNNBasic")
 knn_recommender.train_model()
